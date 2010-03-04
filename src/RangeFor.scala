7ce429fad4217da62984e4f21ad5c754361db9a3
Tommie Gannert <tommie@spotify.com>
2010-03-04 20:29:25 +0100

    Initial commit.

---
+++ src/RangeFor.scala
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2006-2010 Spotify AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package rangefor
+
+import scala.tools.nsc
+import nsc.Global
+import nsc.Phase
+import nsc.plugins.Plugin
+import nsc.plugins.PluginComponent
+import nsc.transform
+
+
+/**
+ * Compiler plugin to transform
+ *
+ *   for (i <- A to|until B [by C]) BLOCK
+ *
+ * to
+ *
+ *   var i = A; while (i <|<= B) { BLOCK; i += C }
+ *
+ * if A happens to be an intWrapper-wrapped term. This is a simple
+ * optimization for a special case that makes Scala code much easier to read.
+ *
+ * @author Tommie Gannert
+ * @date 2010-02-23
+**/
+class RangeFor(val global: Global) extends Plugin {
+	import global._
+
+	val name = "rangefor"
+	val description = "optimise integer for loops"
+	val components = List[PluginComponent](Component)
+
+	/**
+	 * A compiler phase that does the actual transform.
+	**/
+	private object Component extends PluginComponent with transform.Transform with transform.TypingTransformers {
+		val global: RangeFor.this.global.type = RangeFor.this.global
+		val phaseName = RangeFor.this.name
+		val runsAfter = "refchecks"
+
+		def newTransformer(unit: CompilationUnit) = new RangeForTransformer(unit)
+
+		/**
+		 * This transformer rewrites Ident(X) to point to the newly created
+		 * loop variable symbol.
+		**/
+		class IdentTransformer(unit: CompilationUnit, sym: Symbol) extends TypingTransformer(unit) {
+			override def transform(tree: Tree): Tree = {
+				import posAssigner.atPos
+				val NAME = sym.name
+
+				super.transform(tree match {
+				case Ident(NAME) =>
+					atPos(tree.pos) { localTyper.typed { Ident(sym) } }
+
+				case _ =>
+					tree
+				})
+			}
+		}
+
+		/**
+		 * The core transformer, finding matches to the for loop and replacing
+		 * it with an equivalent while loop.
+		**/
+		class RangeForTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
+			override def transform(tree: Tree): Tree = {
+				val NAME_BY = newTermName("by")
+				val NAME_INT_WRAPPER = newTermName("intWrapper")
+
+				/**
+				 * Generate the actual while loop AST.
+				 *
+				 * @param a the start value.
+				 * @param b the ending value (inclusive or exclusive).
+				 * @param func the body of the for loop.
+				 * @param cmp the comparator name (must be a function of scala.Int).
+				 * @param mod the modifier name (incrementer, that must be a function of scala.Int).
+				 * @param modArg the argument to the modifier function, usually the constant 1.
+				 * @return the while tree
+				**/
+				def whileLoop(a: Tree, b: Tree, func: Function, cmp: Name, mod: Name, modArg: Tree) = {
+					import posAssigner.atPos
+
+					val label = newTermName(unit.fresh.newName(tree.pos, "rangefor$"))
+					val labelSym = currentOwner.newLabel(tree.pos, label).setInfo(MethodType(List(), definitions.UnitClass.tpe))
+					val loopvarSym = currentOwner.newVariable(tree.pos, func.vparams(0).name).setInfo(func.vparams(0).symbol.info)
+					val cmpSym = definitions.getMember(definitions.IntClass, cmp).suchThat(_.tpe.paramTypes(0) == a.tpe.underlying)
+					val modSym = definitions.getMember(definitions.IntClass, mod).suchThat(_.tpe.paramTypes(0) == a.tpe.underlying)
+
+					atPos(tree.pos) { localTyper.typed {
+						Block(
+							List(
+								ValDef(loopvarSym, a)
+							),
+							LabelDef(
+								labelSym,
+								List(),
+								If(
+									Apply(
+										Select(Ident(loopvarSym), cmpSym.name).setSymbol(cmpSym),
+										List(b)
+									),
+									Block(
+										List(
+											Block(
+												List(
+													new IdentTransformer(unit, loopvarSym).transform(func.body)
+												),
+												Assign(
+													Ident(loopvarSym),
+													Apply(
+														Select(Ident(loopvarSym), modSym.name).setSymbol(modSym),
+														List(modArg)
+													)
+												)
+											)
+										),
+										Apply(
+											Ident(labelSym),
+											List()
+										)
+									),
+									Literal()
+								)
+							)
+						)
+					} }
+				}
+
+				/**
+				 * Return a comparator name depending on the range builder.
+				**/
+				def nameToCmp(name: Name) = name.toString match {
+					case "to" => nme.LE
+					case "until" => nme.LT
+					case _ => null
+				}
+
+				/**
+				 * Return the while loop if the how is recognizedm otherwise return the original tree.
+				**/
+				def whileOrPass(a: Tree, b: Tree, func: Function, how: Name, modArg: Tree) = {
+					val cmp = nameToCmp(how)
+
+					if (cmp eq null) tree else whileLoop(a, b, func, cmp, nme.ADD, modArg)
+				}
+
+				super.transform(tree match {
+				case Apply(Select(Apply(Select(Apply(Select(Apply(Select(Select(This(nme.scala_tn), nme.Predef), NAME_INT_WRAPPER), List(a)), how), List(b)), NAME_BY), List(c)), nme.foreach), List(func: Function)) =>
+					whileOrPass(a, b, func, how, c)
+
+				case Apply(Select(Apply(Select(Apply(Select(Select(This(nme.scala_tn), nme.Predef), NAME_INT_WRAPPER), List(a)), how), List(b)), nme.foreach), List(func: Function)) =>
+					whileOrPass(a, b, func, how, Literal(Constant(1)))
+
+				case _ =>
+					tree
+				})
+			}
+		}
+	}
+}
